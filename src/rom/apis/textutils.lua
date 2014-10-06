bb3bca5df54686bb9214bab9d7c77650d3ba3474
SoniEx2 <endermoneymod@gmail.com>
2014-10-06 18:27:32 -0300

    Added urlEncode

--- src/rom/apis/textutils.lua
+++ src/rom/apis/textutils.lua
@@ -279,3 +279,14 @@ unserialize = function(s)
       safe = true -- to emulate CC behaviour
     })
 end
+
+-- http://lua-users.org/wiki/StringRecipes
+urlEncode = function(str)
+  if (str) then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w %-%_%.%~])",
+      function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = string.gsub (str, " ", "+")
+  end
+  return str
+end
